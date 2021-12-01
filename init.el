3b882d5cd7999c13a8c2fac3cf321f93d0f0bac9
Rodrigo Campos <rod.apd@gmail.com>
2021-12-01 08:05:02 -0300

    feat(init): add some ui improvements
    - Disable some ui elements
    - Set a nice font
    - Set a nice theme

---
+++ init.el
@@ -0,0 +1,10 @@
+;; disable some ui elements
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; set a nice monospace font
+(set-frame-font "JetBrains Mono-13" nil t)
+
+;; set a nice dark theme
+(load-theme 'modus-vivendi)
