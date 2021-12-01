c9c0618ba33bed2f4d4919b0f7a2f49dc63c164f
Rodrigo Campos <rod.apd@gmail.com>
2021-12-01 08:15:44 -0300

    feat(init): set initial buffer choice to iterate faster

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
 (add-to-list 'load-path (concat user-emacs-directory "lisp/"))
 
+(customize-set-variable 'initial-buffer-choice (expand-file-name (concat user-emacs-directory "init.el")))
+
 (require 'rod-ui)
