7bd437830ca69557d2b7a10f1cdb1115d524302e
Rodrigo Campos <rod.apd@gmail.com>
2021-12-01 22:56:53 -0300

    feat(git): add diff-hl with magit hooks

--- lisp/rod-git.el
+++ lisp/rod-git.el
@@ -1,4 +1,12 @@
 (use-package magit
   :commands (magit-status))
 
+(use-package diff-hl
+  :defer 1
+  :hook
+  ((magit-pre-refresh . diff-hl-magit-pre-refresh)
+   (magit-post-refresh . diff-hl-magit-post-refresh))
+  :config
+  (global-diff-hl-mode 1))
+
 (provide 'rod-git)
