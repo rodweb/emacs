13f088e3fdec045cd024ffb7a4555389066b9b81
Rodrigo Campos <rod.apd@gmail.com>
2021-12-02 08:04:31 -0300

    feat(minibuffer): add orderless

--- lisp/rod-minibuffer.el
+++ lisp/rod-minibuffer.el
@@ -5,4 +5,16 @@
   :config
   (vertico-mode))
 
+(defun rod/use-orderless-in-minibuffer ()
+  (setq-local completion-styles '(orderless)))
+
+(use-package orderless
+  :after vertico
+  :custom
+  (orderless-matching-styles '(orderless-literal
+			       orderless-regexp
+			       orderless-initialism))
+  :hook
+  (minibuffer-setup . rod/use-orderless-in-minibuffer))
+
 (provide 'rod-minibuffer)
