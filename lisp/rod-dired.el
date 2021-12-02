7d25e35019d1223cf86fd125076aa8c5136dcc60
Rodrigo Campos <rod.apd@gmail.com>
2021-12-02 07:27:58 -0300

    feat(dired): enable dired-omit-mode

--- lisp/rod-dired.el
+++ lisp/rod-dired.el
@@ -1,6 +1,7 @@
 (use-package dired
   :straight nil
   :commands (dired dired-jump)
-  :hook (dired-mode . dired-hide-details-mode))
+  :hook ((dired-mode . dired-hide-details-mode)
+	 (dired-mode . dired-omit-mode)))
 
 (provide 'rod-dired)
