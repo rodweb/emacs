0301c9393636802fb6d64e39a560319ad46aa464
Rodrigo Campos <rod.apd@gmail.com>
2021-12-02 21:43:46 -0300

    feat(dired): add binding to wdired-mode

--- lisp/rod-dired.el
+++ lisp/rod-dired.el
@@ -1,6 +1,9 @@
 (use-package dired
   :straight nil
   :commands (dired dired-jump)
+  :bind
+  (:map dired-mode-map
+	("C-c C-q" . #'wdired-change-to-wdired-mode))
   :hook ((dired-mode . dired-hide-details-mode)
 	 (dired-mode . dired-omit-mode)
 	 (dired-mode . diff-hl-dired-mode)))
