30d501b09f9045b739d9ff639944cd08dbc13491
Rodrigo Campos <rod.apd@gmail.com>
2021-12-03 06:46:17 -0300

    feat(search): add custom command to ignore test files

--- lisp/rod-search.el
+++ lisp/rod-search.el
@@ -3,4 +3,13 @@
   :config
   (rg-enable-default-bindings))
 
+(rg-define-search rg-no-tests
+  :format regexp
+  :dir project
+  :files "all"
+  :flags ("--type-add 'notest:*.test.js'"
+	  "--type-add 'notest:*.test.ts'"
+	  "--type-not notest")
+  :menu ("Custom" "n" "No tests"))
+
 (provide 'rod-search)
