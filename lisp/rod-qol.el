8859fa145d2f042e87fd6f8970fdf85aba4561fb
Rodrigo Campos <rod.apd@gmail.com>
2021-12-02 23:14:21 -0300

    feat(qol): write module to file and require on init.el

--- lisp/rod-qol.el
+++ lisp/rod-qol.el
@@ -3,12 +3,18 @@
    NAME is the module suffix."
   (interactive "sName: ")
   (let* ((filename (concat user-emacs-directory "lisp/" "rod-" name ".el"))
-	 (buffer (get-buffer-create filename)))
+         (buffer (get-buffer-create filename)))
+    (with-current-buffer (get-buffer "init.el")
+      (goto-char (point-max))
+      (insert (concat "(require 'rod-" name ")"))
+      (save-buffer))
     (with-current-buffer buffer
       (emacs-lisp-mode)
       (insert "(provide 'rod-" name ")")
       (goto-char (point-min))
       (open-line 2)
+      (write-region nil nil filename)
+      (set-buffer-modified-p nil)
       (switch-to-buffer buffer))))
 
 (use-package files
