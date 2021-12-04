34b74de566b98d9226c27317f7994e6f3a8695fa
Rodrigo Campos <rod.apd@gmail.com>
2021-12-04 06:43:13 -0300

    feat(programming): change default functions to more useful ones

--- lisp/rod-programming.el
+++ lisp/rod-programming.el
@@ -9,4 +9,12 @@
 (show-paren-mode 1)
 (delete-selection-mode 1)
 
+;; better default functions
+(global-set-key (kbd "M-/") #'hippie-expand)
+(global-set-key (kbd "C-x C-b") #'ibuffer)
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
 (provide 'rod-programming)
