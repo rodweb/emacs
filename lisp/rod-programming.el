6bc6e7e116500ccb7a81c6b59028e390ec4f4ee9
Rodrigo Campos <rod.apd@gmail.com>
2021-12-04 06:35:11 -0300

    feat(programming): disable indent-tabs-mode by default

--- lisp/rod-programming.el
+++ lisp/rod-programming.el
@@ -3,6 +3,8 @@
   :config
   (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
 
+(setq-default indent-tabs-mode nil)
+
 (save-place-mode 1)
 (show-paren-mode 1)
 (delete-selection-mode 1)
