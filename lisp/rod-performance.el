d4183993b3984f7c3e5a4a6345b77537a43000f6
Rodrigo Campos <rod.apd@gmail.com>
2021-12-03 22:01:48 -0300

    feat(performance): disable bidirectional display

--- lisp/rod-performance.el
+++ lisp/rod-performance.el
@@ -1,4 +1,8 @@
 (use-package gcmh
   :config (gcmh-mode))
 
+;; disable bidirectional display
+(setq-default bidi-paragraph-direction 'left-to-right
+	      bidi-inhibit-bpa t)
+
 (provide 'rod-performance)
