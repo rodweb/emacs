6dd8c04b1b6f57506087fc73c6f46942054fb83e
Rodrigo Campos <rod.apd@gmail.com>
2021-12-03 21:29:36 -0300

    feat(ui): disable gui dialogs

--- lisp/rod-ui.el
+++ lisp/rod-ui.el
@@ -3,6 +3,10 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
+;; disable gui dialogs
+(setq use-dialog-box nil
+      use-file-dialog nil)
+
 ;; set a nice monospace font
 (set-frame-font "JetBrains Mono-13" nil t)
 
