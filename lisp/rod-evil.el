01b20b225df364192027b796c4dce086ad6e0227
Rodrigo Campos <rod.apd@gmail.com>
2021-12-05 17:58:50 -0300

    feat(evil): make emacs-state the default for dired

--- lisp/rod-evil.el
+++ lisp/rod-evil.el
@@ -1,7 +1,7 @@
 (setq evil-want-keybinding nil)
 
 (use-package evil
-  :hook (after-init . evil-mode)
+  :defer 1
   :custom
   (evil-want-C-i-jump t)
   (evil-want-Y-yank-to-eol t)
@@ -10,7 +10,9 @@
   (evil-symbol-word-search t)
   :config
   (add-hook 'with-editor-mode-hook 'evil-insert-state)
-  (add-hook 'help-mode-hook 'evil-emacs-state))
+  (add-hook 'dired-mode-hook 'evil-emacs-state)
+  (add-hook 'help-mode-hook 'evil-emacs-state)
+  (evil-mode))
 
 (use-package evil-commentary
   :after evil
