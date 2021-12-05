7d4fccadc7a0998cdb97c8f86a6f720b5a5a5e19
Rodrigo Campos <rod.apd@gmail.com>
2021-12-05 08:23:19 -0300

    perf(prog): delay mode initialization

--- lisp/rod-prog.el
+++ lisp/rod-prog.el
@@ -15,20 +15,22 @@
   :bind
   ("C-c ," . quickrun))
 
-(setq-default indent-tabs-mode nil)
-(setq-default show-trailing-whitespace t)
+(add-hook 'after-init-hook
+          (lambda ()
+            (setq-default indent-tabs-mode nil)
+            (setq-default show-trailing-whitespace t)
 
-(save-place-mode 1)
-(show-paren-mode 1)
-(electric-pair-mode 1)
-(delete-selection-mode 1)
+            (save-place-mode 1)
+            (show-paren-mode 1)
+            (electric-pair-mode 1)
+            (delete-selection-mode 1)
 
-;; better default functions
-(global-set-key (kbd "M-/") #'hippie-expand)
-(global-set-key (kbd "C-x C-b") #'ibuffer)
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "C-r") 'isearch-backward-regexp)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
+            ;; better default functions
+            (global-set-key (kbd "M-/") #'hippie-expand)
+            (global-set-key (kbd "C-x C-b") #'ibuffer)
+            (global-set-key (kbd "C-s") 'isearch-forward-regexp)
+            (global-set-key (kbd "C-r") 'isearch-backward-regexp)
+            (global-set-key (kbd "C-M-s") 'isearch-forward)
+            (global-set-key (kbd "C-M-r") 'isearch-backward)))
 
 (provide 'rod-prog)
