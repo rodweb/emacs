fab691e87c139428f1b1f2a47e375eac8be384f1
Rodrigo Campos <rod.apd@gmail.com>
2021-12-06 07:18:03 -0300

    feat(git): add code-review

--- lisp/rod-git.el
+++ lisp/rod-git.el
@@ -6,6 +6,11 @@
 (use-package forge
   :after magit)
 
+(use-package code-review
+  :bind
+  (:map forge-topic-mode-map
+        ("C-c r" . code-review-forge-pr-at-point)))
+
 (use-package diff-hl
   :defer 1
   :hook
