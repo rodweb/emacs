ee0b161d2e31459b6cff69c9080c217b2e3cf81c
Rodrigo Campos <rod.apd@gmail.com>
2021-12-11 07:35:15 -0300

    feat(js): add npm-mode

--- lisp/rod-js.el
+++ lisp/rod-js.el
@@ -2,4 +2,8 @@
 (use-package yaml-mode :defer t)
 (use-package jq-mode :defer t)
 
+(use-package npm-mode
+  :straight (npm-mode :host github :repo "mojochao/npm-mode" :fork t)
+  :hook ((typescript-mode js-mode) . npm-mode))
+
 (provide 'rod-js)
