27433d351b2af32ca329dc67c9abd59e75e87e7f
Rodrigo Campos <rod.apd@gmail.com>
2021-12-13 20:54:17 -0300

    feat(org): add org-capture-templates and org-babel-load-languages

--- lisp/rod-org.el
+++ lisp/rod-org.el
@@ -1,10 +1,19 @@
 (use-package org
   :defer t
+  :hook (org-capture-mode . evil-insert-state)
   :custom
   (org-imenu-depth 4)
   (org-directory "~/org")
   (org-agenda-files '("~/org/inbox.org"))
-  (org-crypt-key "rod.apd@gmail.com"))
+  (org-crypt-key "rod.apd@gmail.com")
+  (org-capture-templates '(("i" "Inbox" entry (file+headline "inbox.org" "Inbox") "** TODO %?")))
+  (org-babel-load-languages '((emacs-lisp . t)
+                              (python . t)
+                              (sql . t)
+                              (shell . t)
+                              (verb . t)))
+  :bind (("C-c n a" . org-agenda)
+         ("C-c n c" . org-capture)))
 
 (use-package org-roam
   :defer t
