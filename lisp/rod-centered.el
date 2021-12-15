57851483e65b6732c452c3ec79ca9e4134c3aaae
Rodrigo Campos <rod.apd@gmail.com>
2021-12-15 13:57:05 -0300

    feat(centered): add centered-cursor-mode

--- lisp/rod-centered.el
+++ lisp/rod-centered.el
@@ -1,4 +1,6 @@
 (advice-add 'evil-next-line :after #'evil-scroll-line-to-center)
 (advice-add 'evil-previous-line :after #'evil-scroll-line-to-center)
 
+(use-package centered-cursor-mode :defer t)
+
 (provide 'rod-centered)
