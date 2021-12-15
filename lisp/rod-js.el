0a2d9a54bf7cd3e65ee22d26b01925f634a91c58
Rodrigo Campos <rod.apd@gmail.com>
2021-12-14 21:59:04 -0300

    feat(js): set tab-width to 2 by default

--- lisp/rod-js.el
+++ lisp/rod-js.el
@@ -6,16 +6,22 @@
   :straight (npm-mode :host github :repo "mojochao/npm-mode" :fork t)
   :hook ((typescript-mode js-mode) . npm-mode))
 
+(use-package indium)
+
+(add-to-list 'compilation-error-regexp-alist-alist
+            '(javascript-stack-trace
+                "(\\([^:]+\\):\\([[:digit:]]+\\):\\([[:digit:]]+\\))"
+                1 2 3 1))
+
+(add-to-list 'compilation-error-regexp-alist 'javascript-stack-trace)
+
+(add-to-list 'compilation-transform-file-match-alist
+            '("internal/.*\\.js" . nil))
+
 (defun rod/setup-js ()
-  (add-to-list 'compilation-error-regexp-alist-alist
-               '(javascript-stack-trace
-                 "(\\([^:]+\\):\\([[:digit:]]+\\):\\([[:digit:]]+\\))"
-                 1 2 3 1))
+  (setq-local tab-width 2))
 
-  (add-to-list 'compilation-error-regexp-alist 'javascript-stack-trace)
-
-  (add-to-list 'compilation-transform-file-match-alist
-               '("internal/.*\\.js" . nil)))
 (add-hook 'js-mode #'rod/setup-js)
+(add-hook 'typescript-mode #'rod/setup-js)
 
 (provide 'rod-js)
