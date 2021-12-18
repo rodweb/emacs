52a411420fff7eef5b3fdd77f87d275f44cc0642
Rodrigo Campos <rod.apd@gmail.com>
2021-12-17 21:02:27 -0300

    feat(lsp): add consult-eglot

--- lisp/rod-lsp.el
+++ lisp/rod-lsp.el
@@ -10,6 +10,9 @@
         ("C-c l o" . eglot-code-action-organize-imports)
         ("C-c l f" . eglot-format-buffer)
         ("C-c l q" . eglot-code-action-quickfix)
-        ("C-c l a" . eglot-code-actions)))
+        ("C-c l a" . eglot-code-actions)
+        ("C-c l s" . consult-eglot-symbols)))
+
+(use-package consult-eglot :after eglot)
 
 (provide 'rod-lsp)
