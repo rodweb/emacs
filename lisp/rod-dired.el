0f354f20fe7a54353691e02b9efc99f3451d9a01
Rodrigo Campos <rod.apd@gmail.com>
2021-12-17 21:01:15 -0300

    feat(dired): add bindings for dired-jump and dired-up-directory

--- lisp/rod-dired.el
+++ lisp/rod-dired.el
@@ -4,17 +4,21 @@
   :init (put 'dired-find-alternate-file 'disabled nil)
   :bind
   (:map dired-mode-map
-	("C-c C-q" . #'wdired-change-to-wdired-mode)
+        ("-" . #'dired-up-directory)
+        ("C-c C-q" . #'wdired-change-to-wdired-mode)
         ("<return>" . #'dired-find-alternate-file))
+  :general
+  (general-nvmap
+    "-" #'dired-jump)
   :hook ((dired-mode . dired-hide-details-mode)
-	 (dired-mode . dired-omit-mode)
-	 (dired-mode . diff-hl-dired-mode)))
+         (dired-mode . dired-omit-mode)
+         (dired-mode . diff-hl-dired-mode)))
 
 (use-package dired-hide-dotfiles
   :after dired
   :bind
   (:map dired-mode-map
-	("." . dired-hide-dotfiles-mode))
+        ("." . dired-hide-dotfiles-mode))
   :config
   (dired-hide-dotfiles-mode))
 
