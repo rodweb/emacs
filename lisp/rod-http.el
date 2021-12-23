9413b1f7fa2a4842200b50bdac9a35059055e6bb
Rodrigo Campos <rod.apd@gmail.com>
2021-12-23 07:13:09 -0300

    feat(http): add verb-mode and support for org-babel

--- lisp/rod-http.el
+++ lisp/rod-http.el
@@ -1,8 +1,14 @@
 (use-package verb
-  :defer t
-  :hook (org-mode . rod/setup-verb-bindings))
+  :hook (org-mode . rod/setup-verb-mode))
 
-(defun rod/setup-verb-bindings ()
+(use-package verb-env
+  :after verb
+  :straight '(:host github :repo "rodweb/verb-env"))
+
+(defun rod/setup-verb-mode ()
+  (org-babel-do-load-languages 'org-babel-load-languages
+                               (append org-babel-load-languages
+                                       '((verb . t))))
   (define-key org-mode-map (kbd "C-c C-r") verb-command-map))
 
 (provide 'rod-http)
