1c0a87b073d43babf39fcd560cbb364bba3c45af
Rodrigo Campos <rod.apd@gmail.com>
2021-12-23 07:14:38 -0300

    feat(project): enable creation of missing test files

--- lisp/rod-project.el
+++ lisp/rod-project.el
@@ -3,6 +3,7 @@
   :custom
   (projectile-generic-command "rg --files --hidden")
   (projectile-project-search-path '("~/dev" "~/dev/gupy"))
+  (projectile-create-missing-test-files t)
   :config
   (projectile-mode))
 
