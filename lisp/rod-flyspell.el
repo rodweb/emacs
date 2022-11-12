a4e4bf8b6491a84c230c6ee95dc534c362b20ce3
Rodrigo Campos <rod.apd@gmail.com>
2022-11-12 07:21:41 -0300

    feat(flyspell): add auto-dicionary-mode

--- lisp/rod-flyspell.el
+++ lisp/rod-flyspell.el
@@ -3,4 +3,8 @@
   :hook ((prog-mode . flyspell-prog-mode)
          (text-mode . flyspell-mode)))
 
+(use-package auto-dicionary-mode
+  :straight (auto-dicitonary-mode :host github :repo "nschum/auto-dictionary-mode" :branch "master")
+  :hook ((flyspell-mode . auto-dictionary-mode)))
+
 (provide 'rod-flyspell)
