0f47a1e57670d10be36c691e0b33c414643c1adc
Rodrigo Campos <rodrigo.campos@messagebird.com>
2023-01-03 12:00:49 -0300

    feat(env): load NPM_TOKEN env

--- lisp/rod-env.el
+++ lisp/rod-env.el
@@ -1,7 +1,7 @@
 (use-package exec-path-from-shell
   :defer 0.1
   :config
-  (add-to-list 'exec-path-from-shell-variables "AWS_SHARED_CREDENTIALS_FILE")
+  (add-to-list 'exec-path-from-shell-variables "AWS_SHARED_CREDENTIALS_FILE" "NPM_TOKEN")
   (exec-path-from-shell-initialize))
 
 (provide 'rod-env)
