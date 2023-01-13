3dedb833e13a2402eb4241b145622061480c356d
Rodrigo Campos <rodrigo.campos@messagebird.com>
2023-01-13 08:28:05 -0300

    feat(flyspell): remove auto-dictionary and set aspell

--- lisp/rod-flyspell.el
+++ lisp/rod-flyspell.el
@@ -1,10 +1,9 @@
 (use-package flyspell
   :straight nil
+  :custom
+  (ispell-program-name "aspell")
+  (ispell-list-command "--list")
   :hook ((prog-mode . flyspell-prog-mode)
          (text-mode . flyspell-mode)))
-
-(use-package auto-dicionary-mode
-  :straight (auto-dicitonary-mode :host github :repo "nschum/auto-dictionary-mode" :branch "master")
-  :hook ((flyspell-mode . auto-dictionary-mode)))
 
 (provide 'rod-flyspell)
