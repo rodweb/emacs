912c9f401f64196d70653725dab760c3b4c1faf9
Rodrigo Campos <rodrigo.campos@messagebird.com>
2023-01-13 08:28:39 -0300

    feat(http): add restclient

--- lisp/rod-http.el
+++ lisp/rod-http.el
@@ -5,6 +5,8 @@
   :straight (verb-env :host github :repo "rodweb/verb-env" :branch "main")
   :hook (verb-mode . verb-env-mode))
 
+(use-package restclient :defer t)
+
 (defun rod/setup-verb-mode ()
   (org-babel-do-load-languages 'org-babel-load-languages
                                (append org-babel-load-languages
