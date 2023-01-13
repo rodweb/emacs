2c326247459b6ed88515a42773bb157d6b0a4d86
Rodrigo Campos <rodrigo.campos@messagebird.com>
2023-01-13 08:34:16 -0300

    feat(org): remove todoist

--- lisp/rod-org.el
+++ lisp/rod-org.el
@@ -42,8 +42,4 @@
   (let ((files (directory-files-recursively org-directory "\\.\\(org\\|gpg\\)$" nil)))
     (find-file (completing-read "Find file: " files))))
 
-(use-package todoist
-  :defer t
-  :config (setq todoist-token (password-store-get "todoist.com/token")))
-
 (provide 'rod-org)
