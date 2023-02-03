d27e8a52b3f37c99b537087d4e24a717b8da1ba0
Rodrigo Campos <rod.apd@gmail.com>
2023-02-03 07:29:03 -0300

    feat(env): fix envs

--- lisp/rod-env.el
+++ lisp/rod-env.el
@@ -1,7 +1,9 @@
 (use-package exec-path-from-shell
   :defer 0.1
   :config
-  (add-to-list 'exec-path-from-shell-variables "AWS_SHARED_CREDENTIALS_FILE" "NPM_TOKEN")
+  (add-to-list 'exec-path-from-shell-variables "AWS_SHARED_CREDENTIALS_FILE")
+  (add-to-list 'exec-path-from-shell-variables "NPM_TOKEN")
+  (add-to-list 'exec-path-from-shell-variables "LSP_USE_PLISTS")
   (exec-path-from-shell-initialize))
 
 (provide 'rod-env)
