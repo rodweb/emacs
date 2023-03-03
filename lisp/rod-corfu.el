17d92efb8c22426a3d3ae072f74c5d533e5f72e1
Rodrigo Campos <rod.apd@gmail.com>
2023-03-02 22:15:19 -0300

    feat(corfu): customize some values

--- lisp/rod-corfu.el
+++ lisp/rod-corfu.el
@@ -3,8 +3,16 @@
   (completion-cycle-threshold 3)
   (read-extended-command-predicate #'command-completion-default-include-p)
   (tab-always-indent 'complete)
-  (corfu-auto t)
   (completion-styles '(basic))
+  (corfu-auto t)
+  (corfu-cycle t)
+  (corfu-preselect 'prompt)
+  :bind
+  (:map corfu-map
+        ("TAB" . corfu-next)
+        ([tab] . corfu-next)
+        ("S-TAB" . corfu-previous)
+        ([backtab] . corfu-previous))
   :init
   (global-corfu-mode))
 
