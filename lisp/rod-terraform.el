64b90ee381848ca1f6d889d13bad3f09115953ef
Rodrigo Campos <rodrigo.campos@messagebird.com>
2023-03-30 17:41:23 -0300

    feat(terraform): add support for corfu with cape

--- lisp/rod-terraform.el
+++ lisp/rod-terraform.el
@@ -4,4 +4,9 @@
   :defer 1
   :init (company-terraform-init))
 
+(add-hook 'terraform-mode-hook (lambda ()
+                                 (setq-local completion-at-point-functions
+                                             (mapcar #'cape-company-to-capf
+                                                     (list #'company-terraform)))))
+
 (provide 'rod-terraform)
