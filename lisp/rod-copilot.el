b94bd64364fd00681b27cf056a76c6f9f36d371f
Rodrigo Campos <rodrigo.campos@messagebird.com>
2023-04-13 17:51:24 -0300

    fix(copilot): add node executable

--- lisp/rod-copilot.el
+++ lisp/rod-copilot.el
@@ -1,5 +1,6 @@
 (use-package copilot
   :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
+  :custom (copilot-node-executable "~/.nvm/versions/node/v16.19.0/bin/node")
   :hook (prog-mode . copilot-mode)
   :bind (:map copilot-mode-map
               ("C-<return>" . copilot-accept-completion)))
